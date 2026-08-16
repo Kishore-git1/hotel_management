10b9359ddad283de789a49950c03d68cf3f732ae
Kishore-git1 <kishore290204@gmail.com>
2026-08-16 18:06:39 +0530

    file saved

---
+++ foodproject/food/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Food
+from .serializers import FoodSerializer
+
+
+class FoodViewSet(viewsets.ModelViewSet):
+
+    queryset = Food.objects.all()
+
+    serializer_class = FoodSerializer
+
+    permission_classes = [IsAuthenticated]
