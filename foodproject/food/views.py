220e41b265301e0bfc050c7ff7bfbe3ac1ecec6d
Kishore-git1 <kishore290204@gmail.com>
2026-08-16 18:35:29 +0530

    Update views.py

--- foodproject/food/views.py
+++ foodproject/food/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+print("hello")
+
 from .models import Food
 from .serializers import FoodSerializer
 
